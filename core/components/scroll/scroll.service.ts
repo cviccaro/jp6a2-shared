9472b1a07958ee41a9de447e3c4d1b897fd7d951
Chris Viccaro <cviccaro@jpenterprises.com>
2017-07-30 22:13:19 -0400

    Code cosmetic change

--- core/components/scroll/scroll.service.ts
+++ core/components/scroll/scroll.service.ts
@@ -7,7 +7,7 @@ import { PageScrollInstance, PageScrollService } from 'ng2-page-scroll';
 export class ScrollService {
 	private _lastScrollPos = 0;
 
-	constructor(@Inject(DOCUMENT) private document: any, private scroller: PageScrollService) {
+	constructor( @Inject(DOCUMENT) private document: any, private scroller: PageScrollService) {
 		window.addEventListener('scroll', (e) => {
 			if (window.pageYOffset !== 0) {
 				this._lastScrollPos = window.pageYOffset;
@@ -19,32 +19,31 @@ export class ScrollService {
 		return this._lastScrollPos;
 	}
 
-	scrollToElementAnimated(targetEl: HTMLElement|string) {
+	scrollToElementAnimated(targetEl: HTMLElement | string) {
 		const instance = PageScrollInstance.simpleInstance(this.document, targetEl);
 		this.scroller.start(instance);
 	}
 
-	scrollElementInline(inlineEl: HTMLElement, targetEl: HTMLElement|string) {
+	scrollElementInline(inlineEl: HTMLElement, targetEl: HTMLElement | string) {
 		const instance = PageScrollInstance.simpleInlineInstance(this.document, targetEl, inlineEl);
 		this.scroller.start(instance);
 	}
 
-	navHelper(e: Event) {
-		  e.stopPropagation();
-		  e.preventDefault();
-		  const target = <HTMLElement>e.target;
-		  let href:string;
+	navLinkHelper(e: Event) {
+		e.stopPropagation();
+		e.preventDefault();
+		const target = <HTMLElement>e.target;
+		let href: string;
 
-		  if (target.tagName === 'SPAN') {
-		    href = (<HTMLAnchorElement>target.parentElement).href;
-		  } else {
-		    href = (<HTMLAnchorElement>target).href;
-		  }
+		if (target.tagName === 'SPAN') {
+			href = (<HTMLAnchorElement>target.parentElement).href;
+		} else {
+			href = (<HTMLAnchorElement>target).href;
+		}
 
-		  if (href) {
-		    let selector = href.replace(window.location.origin, '').replace('/','');
-		    this.scrollToElementAnimated('#' + selector);
-		  }
+		if (href) {
+			let selector = href.replace(window.location.origin, '').replace('/', '');
+			this.scrollToElementAnimated('#' + selector);
 		}
 	}
 }
